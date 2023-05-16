9cadc8b5e68d6b8225b3deba770d2997d9ec951f
Durin TASONDOCK <temgoua484@gmail.com>
2023-05-16 08:40:47 +0100

    setting home metas

---
+++ metas/home.js
@@ -0,0 +1,13 @@
+export default {
+  title: 'Nova Rebelle',
+  metas: [
+    {
+      name: "description",
+      content: "Nova Rebelle, Revellez votre vrai potentiel d'artiste"
+    },
+    {
+      property: 'og:image',
+      content:"/favicon.png"
+    }
+  ]
+}
---
+++ metas/index.js
@@ -0,0 +1,1 @@
+export { default as homeMetas } from './home'
